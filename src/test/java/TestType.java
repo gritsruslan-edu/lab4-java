f75f04b1972592b140fc4df5312fc9026c03e353
gritsruslan <gritsruslan@gmail.com>
2026-03-23 15:00:44 +0200

    test fixes

--- src/test/java/TestType.java
+++ src/test/java/TestType.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.sumdu.Book;
+import org.sumdu.Genre;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,7 +8,7 @@ class BookTests {
 
     @Test
     void shouldThrowExceptionWhenInvalidYearInSetter() {
-        Book book = new Book("title", "author", 2000, 1000, "genre");
+        Book book = new Book("title", "author", 2000, 1000, Genre.DETECTIVE);
 
         assertThrows(IllegalArgumentException.class, () -> {
             book.setYear(3000);
@@ -17,7 +18,7 @@ class BookTests {
     @Test
     void shouldThrowExceptionWhenInvalidConstructorData() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new Book("", "", 2000, 100000, "genre");
+            new Book("", "", 2000, 100000, Genre.DETECTIVE);
         });
     }
 }
