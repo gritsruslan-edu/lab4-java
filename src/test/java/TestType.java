74915600b728f08a3c9b9d2cd3af50e3646ad5ff
gritsruslan <gritsruslan@gmail.com>
2026-03-23 13:07:36 +0200

    add unit tests lab 5

---
+++ src/test/java/TestType.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.sumdu.Book;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTests {
+
+    @Test
+    void shouldThrowExceptionWhenInvalidYearInSetter() {
+        Book book = new Book("title", "author", 2000, 1000, "genre");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            book.setYear(3000);
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionWhenInvalidConstructorData() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Book("", "", 2000, 100000, "genre");
+        });
+    }
+}
